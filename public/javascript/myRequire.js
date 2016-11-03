ced692eea1eb06fd90da1af1c97e039a27e94615
ali sait bilgi <alisaitbilgi@gmail.com>
2016-11-03 16:07:57 +0200

    Update myRequire.js

--- public/javascript/myRequire.js
+++ public/javascript/myRequire.js
@@ -6,15 +6,7 @@ function require(fileName) {
 	xhr.onreadystatechange = function() {
 		if(xhr.readyState === 4) {
 			if(xhr.status === 200){
-				/* bugün bu saate kadar evalsiz bu kodu işletmeye çalıştım fakat beceremedim, bu kısmı
-				new Function şeklinde bıraktım ama aslında "eval(xhr.responseText);" ile aynı işlem.
-				new Function'ın nasıl kullanıldığını sen gösterdikten sonra buraya uyarlamaya çalıştım
-				fakat function body'sine module.exports.a = {a:5}; şeklinde yazığımızda çalışıyor ancak
-				mesela var x = module.exports.a = {a:5}; dedikten sonra new Function'ın body'sine x'i yazığımda,
-				bu x'in eşit olduğu işlemi yani yaptıramadım.. :(  Neden yazdıramadığımı biliyorum anladım fakat nasıl
-				yazdırmam gerektiğini bulamadım o yüzden daha fazla zaman kaybetmeden eval'li olan versiyonuyla
-				bıraktım.. Check'lerin eklenmiş halini de , eğer oluyor evalsiz yazılan halini de yazabilirim */
-				eval(new Function("module", "response", "return response")(module, xhr.responseText));
+				new Function("module", xhr.responseText)(module);
 			}
 		}
 	}
